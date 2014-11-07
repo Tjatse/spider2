8c98b52520b2dd0a8e9b8d46eab3ecb6feec2c3a
Tjatse <thisnamemeansnothing@gmail.com>
2014-11-07 16:28:47 +0800

    basic example

--- examples/basic.js
+++ examples/basic.js
@@ -1,29 +1,28 @@
-var Spider = require('../');
+var path = require('path'),
+  _ = require('lodash'),
+  Spider = require('../');
 
 var spider = Spider({
   debug: true,
-  workers: 2,
-  concurrency: 1,
-  /**
-   * must be:
-   * 1. uri must have 4 digital at least
-   * 2. uri can not be a bitmap
-   * 3. uri can not have no path
-   * 4. length of title must greater than 5
-   */
-  predication: function(ele){
-    var qsi, uri = ele.uri;
-    if((qsi = uri.indexOf('?')) > 0){
-      uri = uri.substr(0, qsi);
-    }
-    return uri.match(/\d{4,}/i) && !uri.match(/\.(jpg|png|jpeg|pdf)/i) && uri.indexOf('/') != uri.length - 1 && ele.title.length >= 5;
+  workers: 7,
+  concurrency: 1
+});
+spider.on('error', function (err, req) {
+  if (req.worker) {
+    console.error('worker #', req.worker, 'has an error:', err.message);
+  } else {
+    console.error(req.uri, err.message);
   }
 });
-spider.on('error', function(err, data){
-  console.log('[ERROR]', err.message, data.uri);
+spider.on('data', function (req, res) {
+  if (req._type == Spider.type.LINK) {
+    spider.read(_.filter(res, validLink));
+  } else if (req._type == Spider.type.ARTICLE) {
+    console.log(req.uri, res.title);
+  }
 });
-spider.on('data', function(type, data){
-  console.log('[' + type + ']', data.uri, data.title);
+spider.on('end', function () {
+  console.log('[END]');
 });
 
 spider.crawl([
@@ -35,20 +34,37 @@ spider.crawl([
 
 /*
 setTimeout(function(){
-  spider.crawl([
-    'http://getbootstrap.com/components/',
-    'https://lodash.com/docs#compact',
-    'https://www.npmjs.org/package/read-art'
-  ]);
-}, 500);
-*/
+ spider.crawl([
+ 'http://getbootstrap.com/components/',
+ 'https://lodash.com/docs#compact',
+ 'https://www.npmjs.org/package/read-art'
+ ]);
+}, 200);*/
 
 /*
-setTimeout(function(){
-  spider.destroy();
-}, 500);*/
-/*
-setTimeout(function(){
-  var pong = spider.ping();
-  console.log(pong + ' spiders.');
-}, 3000);*/
+ setTimeout(function(){
+ spider.destroy();
+ }, 500);*/
+
+ setTimeout(function(){
+   var pong = spider.ping();
+   console.log(pong);
+ }, 20000);
+
+function validLink(ele){
+  if(!ele.uri || !ele.title){
+    return false;
+  }
+  /**
+   * must be:
+   * 1. uri must have 4 digital at least
+   * 2. uri can not be a bitmap
+   * 3. uri can not have no path
+   * 4. length of title must greater than 5
+   */
+  var qsi, uri = ele.uri;
+  if ((qsi = uri.indexOf('?')) > 0) {
+    uri = uri.substr(0, qsi);
+  }
+  return uri.match(/\d{4,}/i) && !uri.match(/\.(jpg|png|jpeg|pdf)/i) && uri.indexOf('/') != uri.length - 1 && ele.title.length >= 5;
+}
